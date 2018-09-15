596ded8b2b43a9b4f7f3420f80deffd3091a2716
Astronaut Group <19156046+alexanderjbeal@users.noreply.github.com>
2018-09-15 10:16:54 -0500

    Concept Update

--- assets/javascript/menu.js
+++ assets/javascript/menu.js
@@ -1,20 +1,21 @@
-var menuIcon = $(".nav-menu");  
-var menuToggle = new TimelineMax({paused:true, reversed:true});
-var slideDown = $(".menu-container");
-var listItems = $(".list-items > li");
+let body = $(".header");
+var menuIcon = $(".navigation__button");  
+var menuToggle = new TimelineMax({ paused:true, reversed:true });
+var slideDown = $(".navigation__background");
+var listItems = $(".navigation__list > li");
 
 TweenMax.set(slideDown, {
-      y: '-100%'
-  });
+      y: '-100%',
+  }, .15);
 
 menuToggle  
   .to(menuIcon, .5, {
-  x:'-10',
+  x:'5',
   ease: Back.easeOut
   // ease: Back.easeIn.config(2.2)
   })
-  .to(slideDown, 1, {
-  y: '0%',
+  .to(slideDown, .5, {
+  y: '-10%',
   ease: Back.easeOut
   })
   .staggerFrom(listItems, .25, {
@@ -24,12 +25,55 @@ menuToggle
 
 menuIcon.click(function () {
   menuToggle.reversed() ? menuToggle.restart() : menuToggle.reverse();
-    // $('.nav-menu i').css('color','white');
 });
+
+// body.click(function () {
+//   menuToggle.reversed() ? menuToggle.restart() : menuToggle.reverse();
+//     // $('.nav-menu i').css('color','white');
+// });
 
 listItems.click(function () {
   menuToggle.reversed() ? menuToggle.restart() : menuToggle.reverse();
-  $(document).ready(function() {
-    
-  })
 });
+
+
+
+
+
+
+
+// var menuIcon = $(".nav-menu");  
+// var menuToggle = new TimelineMax({paused:true, reversed:true});
+// var slideDown = $(".menu-container");
+// var listItems = $(".list-items > li");
+
+// TweenMax.set(slideDown, {
+//       y: '-100%'
+//   });
+
+// menuToggle  
+//   .to(menuIcon, .5, {
+//   x:'-10',
+//   ease: Back.easeOut
+//   // ease: Back.easeIn.config(2.2)
+//   })
+//   .to(slideDown, 1, {
+//   y: '0%',
+//   ease: Back.easeOut
+//   })
+//   .staggerFrom(listItems, .25, {
+//     y: '-10',
+//     ease: Power1.easeOut
+//   }, .1);
+
+// menuIcon.click(function () {
+//   menuToggle.reversed() ? menuToggle.restart() : menuToggle.reverse();
+//     // $('.nav-menu i').css('color','white');
+// });
+
+// listItems.click(function () {
+//   menuToggle.reversed() ? menuToggle.restart() : menuToggle.reverse();
+//   $(document).ready(function() {
+    
+//   })
+// });
